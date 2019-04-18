8fcd04823b379d6443c9a8b1fdbd40ad8c7ff5d3
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:02:27 +0545

    my frist

--- DictionaryApp/app/src/main/java/com/dictionaryapp/MainActivity.java
+++ DictionaryApp/app/src/main/java/com/dictionaryapp/MainActivity.java
@@ -2,12 +2,60 @@ package com.dictionaryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String countries[] = {
+            "Nepal", "Kathmandu",
+            "India", "New Delhi",
+            "Japan", "Tokyo",
+            "UK","London",
+            "US","Washington DC",
+            "Canada","Ottawa"
+    };
+
+    
+ private Map<String,String> dictionary;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lstCountries = findViewById(R.id.lstCountries);
+
+        dictionary  = new HashMap<>();
+        for (int i=0;i<countries.length;i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<>(dictionary.keySet())
+
+        );
+        lstCountries.setAdapter(countryAdapter);
+
+
+
+        lstCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                String Capital = dictionary.get(country);
+                Toast.makeText(getApplicationContext(),Capital,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
